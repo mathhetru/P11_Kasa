0f7a6db3218a195f9b665fb0aca2fc90d8d675f6
Mathilde Hetru <mathilde.hetru@adeo.com>
2024-01-16 17:00:02 +0100

    responsive ok

--- src/pages/ErrorView.jsx
+++ src/pages/ErrorView.jsx
@@ -9,7 +9,7 @@ function ErrorPage() {
   return (
     <div className="error">
       <h1 className="error__title">404</h1>
-      <p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
+      <p className="error__text">Oups ! La page que vous demandez n'existe pas.</p>
       <Link to={`/`} className="error__link">
         Retourner sur la page d'accueil
       </Link>
